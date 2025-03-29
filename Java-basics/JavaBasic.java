e8e9444e65b6cd0cf290415cdeb2b5ad01635134
mukeshy2005 <my653197@gmail.com>
2025-03-29 20:40:16 +0530

    add

---
+++ Java-basics/JavaBasic.java
@@ -0,0 +1,9 @@
+public class javaBasic {
+    public static void main (String args[]){
+        System.out.println("hellow world ");
+        System.out.print("hellow world ");
+        System.out.print("hellow world \n ");
+        System.out.print("hellow world  ");
+
+    }
+}
