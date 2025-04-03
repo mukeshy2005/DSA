eb040aec8e4b635607ab45a4b10b0eaff61a5cd6
mukeshy2005 <my653197@gmail.com>
2025-04-03 20:11:31 +0530

    TypeCating

---
+++ Java-basics/TypeCasting.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class TypeCasting{
+    public static void main(String args[]){
+    int a=10;
+    long b=a;
+    System.out.println(b); // implicit type casting
+    // implicit type casting means converting a smaller data type to a larger data type
+    }
+}
